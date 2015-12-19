f1ea4a0fff779bfe692a3db9828668f011531956
Samuel Simões <slip.cm@gmail.com>
2015-12-19 17:21:04 -0200

    Removing the useless code

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require("express"),
-    app = express();
-
-app.use(express.static("public"));
-app.use("/bower_components",  express.static(__dirname + "/bower_components"));
-app.use("/node_modules",  express.static(__dirname + "/node_modules"));
-
-var server = app.listen(process.env.PORT || 3000);
